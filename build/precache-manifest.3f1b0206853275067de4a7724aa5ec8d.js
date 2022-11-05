e7c3644a5f5b86a8177ecbe485571ea15bde94d7
Subhash Reddy <subhash8111999@gmail.com>
2022-11-06 03:06:04 +0530

    Add files via upload

---
+++ build/precache-manifest.3f1b0206853275067de4a7724aa5ec8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6a998155d7d09da9a940f672a6fbb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "738c4f6526b0dfd7f822",
+    "url": "./static/css/main.041490c3.chunk.css"
+  },
+  {
+    "revision": "f87d6e24c328c5a79d1c",
+    "url": "./static/js/2.1fef0211.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1fef0211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738c4f6526b0dfd7f822",
+    "url": "./static/js/main.db183caa.chunk.js"
+  },
+  {
+    "revision": "3515e2cd2f35663fbbac",
+    "url": "./static/js/runtime-main.3550d342.js"
+  }
+]);
